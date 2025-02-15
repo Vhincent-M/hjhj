0594e52f0268e092342d2e2f33243e36a71018f9
vhincent <emijares2021@student.nbscollege.edu.ph>
2025-02-15 19:01:13 +0800

    harbe

--- python_exercise/harb.py
+++ python_exercise/harb.py
@@ -12,35 +12,70 @@ while True:
     
         if choice == "quit":
             print("Thanks for playing!")
-            break
+            exit()  
         elif choice == 'x':
-            print("You died of dehydration.")
+            print("\nYou died of dehydration.")
             print("The End.")
-            break
+            break  
         elif choice == 'y':
-            print("You found a magical trident!")
+            print("\nYou found a magical trident!")
             health += 20
             print(f"Your health is now {health}.")
+            break 
         else:
-            print("Invalid choice.")
-    if choice.lower() == "quit":
-        break
+            print("Invalid choice. Please choose 'x' or 'y'.")
+    
+    if choice == 'x':
+        play_again = input("\nDo you want to play again? (yes/no): ").lower()
+        if play_again != "yes":
+            print("Thanks for playing!")
+            break
+        else:
+            continue
     
     while True:    
         choice = input("Hunt the Megalodon(x) or fight Poseidon(y)? (type 'quit' to exit): ").lower()
     
         if choice == "quit":
             print("Thanks for playing!")
+            exit()
             break
         elif choice == 'x':
             print("You killed the Megalodon but lost some health.")
             health -= 50
             print(f"Your health is now {health}.")
+            break
         elif choice == 'y':
             print("You lost to Poseidon and barely escaped.")
             health -= 70
             print(f"Your health is now {health}.")
+            break
         else:
-            print("Invalid choice.")  
+            print("Invalid choice.") 
+            
+    while True:    
+        choice = input("Seek help from Aquaman(x) or Namor(y)? (type 'quit' to exit): ").lower()
     
+        if choice == "quit":
+            print("Thanks for playing!")
+            exit()
+            break
+        elif choice == 'x':
+            print("Aquaman handed you an Atlantean armor. You gained 50 health.")
+            health += 50
+            print(f"Your health is now {health}.")
+            break
+        elif choice == 'y':
+            print("Namor killed you just because he wanted to.")
+            break
+        else:
+            print("Invalid choice.")
+            continue 
+
     print("The End.")
+         
+
+    play_again = input("Do you want to play again? (yes/no): ").lower()
+    if play_again != "yes":
+        print("Thanks for playing!")
+        break
